9ae34ea073431c1a64449b81eea1c10215b8897b
jstirling91 <Jordan_S@Jordans-MacBook-Pro.local>
2013-10-15 17:54:33 -0400

    debug sfs_mkdir()

--- fs.c
+++ fs.c
@@ -44,7 +44,7 @@ static blkid sfs_alloc_block()
             if((freemap[i] & j) == 0){
                 freemap[i] = freemap[i] | j;
                 sfs_flush_freemap();
-                printf("HERE!!!!");
+                printf("blkid %d\n", i*32 + temp);
                 return i*32 + temp;
             }
             else{
