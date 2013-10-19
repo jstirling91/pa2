ec0d9179ff61e59c98d1595674d21a71a9819d2e
jstirling91 <Jordan_S@wpa168020.wireless.mcgill.ca>
2013-10-19 14:51:21 -0400

    DEBUG sfs_open

--- fs.c
+++ fs.c
@@ -255,7 +255,7 @@ int sfs_rmdir(char *dirname)
     sfs_read_block(&dirRead, dir);
     int i;
     for(i = 0; i < SFS_DB_NINODES; i++){
-        if(dirRead.inodes[i] == 0){
+        if(dirRead.inodes[i] > 0){
             return -1;
         }
     }
