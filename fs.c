035ffc8cce46dd1b7ea2ac75598fdfdab4ab00af
jstirling91 <Jordan_S@wpa168020.wireless.mcgill.ca>
2013-10-19 14:55:52 -0400

    DEBUG sfs_open

--- fs.c
+++ fs.c
@@ -358,8 +358,9 @@ int sfs_open(char *dirname, char *name)
     
 	/* TODO: create a new file */
     inode_bid = sfs_alloc_block();
-    (*inode).size = 0;
     printf("HERE\n");
+    (*inode).size = 0;
+    
     (*inode).first_frame = -1;
     strcpy((*inode).file_name, name);
     sfs_write_block(inode, inode_bid);
