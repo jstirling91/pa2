9ddbb01e6948fb4f1970f0c791fcce551aeb4bbb
jstirling91 <Jordan_S@wpa168020.wireless.mcgill.ca>
2013-10-19 14:54:24 -0400

    DEBUG sfs_open

--- fs.c
+++ fs.c
@@ -351,17 +351,17 @@ int sfs_open(char *dirname, char *name)
         }
         else if(free == -1){
             free = i;
-            printf("HERE   k\n");
         }
     }
     if(free == -1)
         return -1;
-    printf("HERE\n");
+    
 	/* TODO: create a new file */
     inode_bid = sfs_alloc_block();
     (*inode).size = 0;
     (*inode).first_frame = -1;
     strcpy((*inode).file_name, name);
+    printf("HERE\n");
     sfs_write_block(inode, inode_bid);
     dir.inodes[free] = inode_bid;
     sfs_write_block(&dir, dir_bid);
