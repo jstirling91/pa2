a682dc22d1ebc14f9ac8825c18ce9367ab73c5d9
jstirling91 <jstirling91@gmail.com>
2013-10-20 00:46:56 -0400

    DEBUG test 7

--- fs.c
+++ fs.c
@@ -196,7 +196,7 @@ static blkid sfs_find_dir(char *dirname)
         if(strcmp(dirname, dir.dir_name) == 0)
             return dir_bid;
         int i = 0;
-        while(dir.next_dir != 0 && i < 10){
+        while(dir.next_dir != 0){
             dir_bid = dir.next_dir;
             sfs_read_block(&dir, dir_bid);
             i++;
