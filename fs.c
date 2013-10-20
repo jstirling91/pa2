750dd03a44ef01797214a9ec376b118103c993d0
jstirling91 <jstirling91@gmail.com>
2013-10-20 19:32:14 -0400

    DEBUG test 10

--- fs.c
+++ fs.c
@@ -182,10 +182,11 @@ static u32 sfs_get_file_content(blkid *bids, int fd, u32 cur, u32 length)
 //    ////printf("START: %d, FINISH: %d\n", start, end);
     for(i = start; i <= end; i++){
         if(i % SFS_FRAME_COUNT == 0 && i != 0){
+            printf("HERE: %d\n", i);
             sfs_write_block(&frame, temp);
             temp = frame.next;
             sfs_read_block(&frame, temp);
-            printf("HERE: %d\n", i);
+            
         }
         if(frame.content[i % SFS_FRAME_COUNT] == 0){
             
