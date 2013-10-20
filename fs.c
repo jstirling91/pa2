a809b8efe696d0405ca0a142da2f4f28625499d6
jstirling91 <Jordan_S@wpa168024.wireless.mcgill.ca>
2013-10-19 21:58:22 -0400

    DEBUG test 4

--- fs.c
+++ fs.c
@@ -567,7 +567,7 @@ int sfs_read(int fd, void *buf, int length)
         if(i == 0){
             sfs_read_block(&tmp, *(bids));
             memcpy(p, &(tmp[cur % BLOCK_SIZE]), (cur + length) % BLOCK_SIZE);
-            printf("buf: %s\n", p);
+            printf("buf: %s\n", tmp);
             length_left = length - ((cur + length) % BLOCK_SIZE);
         }
         else{
