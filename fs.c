061b796673812d78bd557e9af710deb9be79173c
jstirling91 <Jordan_S@wpa168024.wireless.mcgill.ca>
2013-10-19 22:05:37 -0400

    DEBUG test 4

--- fs.c
+++ fs.c
@@ -508,7 +508,7 @@ int sfs_write(int fd, void *buf, int length)
     n = (cur + length) % BLOCK_SIZE;
     bids = (int *)malloc(n);
     sfs_get_file_content(bids, fd, cur, length);
-    printf("buf1: %d\n", cur);
+    printf("buf1: %d\n", n);
 	/* TODO: main loop, go through every block, copy the necessary parts
 	   to the buffer, consult the hint in the document. Do not forget to 
 	   flush to the disk.
@@ -560,7 +560,7 @@ int sfs_read(int fd, void *buf, int length)
     n = (cur + length) % BLOCK_SIZE;
     bids = (int *)malloc(n);
     sfs_get_file_content(bids, fd, cur, length);
-    printf("buf: %d\n", cur);
+    printf("buf: %d\n", n);
     
     int length_left = length;
     
