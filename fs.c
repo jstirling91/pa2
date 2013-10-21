ef6ee40cd8ab4e72ebda344734dd982daae0e94f
jstirling91 <jstirling91@gmail.com>
2013-10-20 22:13:53 -0400

    DEBUG test 10

--- fs.c
+++ fs.c
@@ -166,7 +166,6 @@ static u32 sfs_get_file_content(blkid *bids, int fd, u32 cur, u32 length)
 	   Transverse the frame list if needed
 	*/
     
-    printf("HERE\n");
     start = cur / BLOCK_SIZE;
     end = (cur + length) / BLOCK_SIZE;
     temp = fdtable[fd].inode.first_frame;
@@ -179,6 +178,8 @@ static u32 sfs_get_file_content(blkid *bids, int fd, u32 cur, u32 length)
     }
     ii = 0;
     for(i = start; i <= end; i++){
+        
+        printf("HERE\n");
         if(i % SFS_FRAME_COUNT == 0 && i != 0){
             sfs_write_block(&frame, temp);
             temp = frame.next;
